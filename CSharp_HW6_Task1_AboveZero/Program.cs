82034058f69cb689f92eb04c1b222e3c241def69
Dashch <dashch88@gmail.com>
2022-12-02 15:49:02 +0400

    Added Task 1. Finding amount of elements above zero.

---
+++ CSharp_HW6_Task1_AboveZero/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] FillArrayByUser(int sizeOfArray)
+{
+    int[] arr = new int[sizeOfArray];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-е число массива: ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int AmountOfElementsAboveZero(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите длину массива: ");
+int[] array = FillArrayByUser(int.Parse(Console.ReadLine()));
+
+PrintArray(array);
+
+Console.WriteLine($"Количество чисел в массиве больше нуля равна: {AmountOfElementsAboveZero(array)}");
